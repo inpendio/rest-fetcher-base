c3807727c5d1830b510ca58eb6f862dc12135ef5
danil fnf <danil@flownform.com>
2019-08-01 10:59:35 +0200

    upgraded, build

--- dist/index.js
+++ dist/index.js
@@ -38,20 +38,35 @@
     return obj;
   }
 
-  function _objectSpread(target) {
+  function ownKeys(object, enumerableOnly) {
+    var keys = Object.keys(object);
+
+    if (Object.getOwnPropertySymbols) {
+      var symbols = Object.getOwnPropertySymbols(object);
+      if (enumerableOnly) symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+      });
+      keys.push.apply(keys, symbols);
+    }
+
+    return keys;
+  }
+
+  function _objectSpread2(target) {
     for (var i = 1; i < arguments.length; i++) {
       var source = arguments[i] != null ? arguments[i] : {};
-      var ownKeys = Object.keys(source);
 
-      if (typeof Object.getOwnPropertySymbols === 'function') {
-        ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
-          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
-        }));
+      if (i % 2) {
+        ownKeys(source, true).forEach(function (key) {
+          _defineProperty(target, key, source[key]);
+        });
+      } else if (Object.getOwnPropertyDescriptors) {
+        Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+      } else {
+        ownKeys(source).forEach(function (key) {
+          Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+        });
       }
-
-      ownKeys.forEach(function (key) {
-        _defineProperty(target, key, source[key]);
-      });
     }
 
     return target;
@@ -166,7 +181,7 @@
             leftovers[key] = request[key];
           }
         } else if (key === 'GET') {
-          leftovers = _objectSpread({}, leftovers, request[key]);
+          leftovers = _objectSpread2({}, leftovers, {}, request[key]);
         }
       });
     }
@@ -385,7 +400,7 @@
         }
 
         return Promise.resolve(json[0]);
-      }).catch(function (e) {
+      })["catch"](function (e) {
         if (_this.dispatch && _this.actionError) {
           _this.dispatch(_this.actionError(name, res, e.message));
         }
